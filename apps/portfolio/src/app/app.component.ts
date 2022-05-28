477327098b18479cedea4bd2f9c4ed6795af0ad9
madison <17911991+MadisonBytes@users.noreply.github.com>
2022-05-28 12:41:32 -0400

    test?

--- apps/portfolio/src/app/app.component.ts
+++ apps/portfolio/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'portfolio-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'portfolio-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  // comment
-  title = 'portfolio';
+    title = 'portfolio';
 }
